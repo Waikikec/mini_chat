774824a671476fae70f1df03e7fcd7fc93d17d35
Waikikec <martin.d.yankov@gmail.com>
2022-09-19 15:17:42 +0300

    Fetch Chat Private Messages

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -4,10 +4,13 @@ import { doc, onSnapshot } from 'firebase/firestore';
 import { db } from '../firebase';
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import { ChatContext } from '../context/ChatContext';
 
 const Chats = () => {
   const [chats, setChats] = useState([]);
+
   const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
 
   useEffect(() => {
     const getChats = () => {
@@ -23,12 +26,18 @@ const Chats = () => {
     currentUser.uid && getChats();
   }, [currentUser.uid]);
 
-  console.log(Object.entries(chats));
+  const handleSelect = (u) => {
+    dispatch({ type: 'CHANGE_USER', payload: u });
+  };
 
   return (
     <div className='chats'>
       {Object.entries(chats)?.map((chat) => (
-        <div className='userChat' key={chat[0]}>
+        <div
+          className='userChat'
+          key={chat[0]}
+          onClick={() => handleSelect(chat[1].userInfo)}
+        >
           <img src={chat[1].userInfo.photoURL} alt='' />
           <div className='userChatInfo'>
             <span>{chat[1].userInfo.displayName}</span>
--- src/components/SingleMessage.jsx
+++ src/components/SingleMessage.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { ChatContext } from '../context/ChatContext';
 
-const SingleMessage = () => {
+const SingleMessage = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
   return (
     <div className='message owner'>
       <div className='messageInfo'>
         <img
-          src='https://images.pexels.com/photos/13304393/pexels-photo-13304393.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+          src={
+            'https://images.pexels.com/photos/13304393/pexels-photo-13304393.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+          }
           alt=''
         />
         <span>Just now</span>
